8f54de203d56662f44a8ef5c44311d8e41e5ca4a
Hebert Salvador cordero <hebertrobsal@gmail.com>
2026-08-20 23:24:37 -0500

    Prueba Final realizada por HEBERT SALVADOR CORDERO

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,57 @@
 package org.example;
-
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+ public class Main {
+    static int obtenerFactorial(int n,int i)
+    {   int factorial=1;
+        for(i=1;i<=n;i++)
+        { factorial=factorial*i;}
+        return factorial;}
+   static int obtenerPotencia(int n)
+    {   int potencia=n*n;
+        return potencia;}
+    static int obtenerSuma(int n)
+    {   int suma=0;
+        for(int i=1;i<=n;i++)
+        { suma=suma+i;}
+        return suma;}
+    public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+        Scanner teclado=new Scanner(System.in);
+        //DECLARACION DE VARIABLES
+        int opcion,factorial,suma=0,i=1,k,n,potencia;
+        //ENTRADA DE DATOS
+        System.out.println("MENU DE OPCIONES:\n\n");
+        System.out.println("1.-Factorial de un numero entero:\n");
+        System.out.println("2.-Suma de n numeros naturales:\n");
+        System.out.println("3.-Potencia al cuadrado de un numero entero:\n");
+        System.out.println("ELIJA UNA OPCION:\n");
+
+        opcion=teclado.nextInt();
+        switch(opcion)
+        {
+            case 1:
+            System.out.println("Ingrese el valor de n:");
+                 n=teclado.nextInt();
+                factorial=obtenerFactorial(n,i);
+                System.out.println("El factirial de n es n!="+factorial);
+                break;
+            case 2:
+             System.out.println("Ingrese el valor de n:");
+                n=teclado.nextInt();
+                suma=obtenerSuma(n);
+                System.out.println("La suma de la serie de n numeros naturales es:"+suma);
+                break;
+            case 3:
+                System.out.println("Ingrese el valor de n:");
+                n=teclado.nextInt();
+                potencia=obtenerPotencia(n);
+                System.out.println("La potencia al cuadrado de n es:"+potencia);
+                default :
+             System.out.println("¡OPCION NO VALIDA!");
         }
+
+
     }
 }
